46f55e89e45297a88a7e35ac9f81bea74db3ccaf
kireeti <kireeti@accendo.com.my>
2019-06-14 11:30:14 +0800

    debug

--- api/routes/sports.js
+++ api/routes/sports.js
@@ -3,13 +3,14 @@ const router = express.Router();
 
 const Sports = require('../models/sportsModel');
 
-router.get('/get', (req, res) => {
+router.get('/get', (req, res, next) => {
     Sports.find((err, result) => {
         if (err) throw err;
         res.status(200).json({
             data: result
         });
     });
+    next();
 });
 
 module.exports = router;
