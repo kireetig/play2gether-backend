c41bc9cda976fe9e7441dcf0a219c8f5289d92dc
kireeti <kireeti@accendo.com.my>
2019-06-25 15:47:54 +0800

    minor change

--- api/routes/game.js
+++ api/routes/game.js
@@ -5,24 +5,31 @@ const checkToken = require('../middleware/check-auth');
 const Game = require('../models/sportsModel');
 
 router.post('/host', checkToken, (req, res, next) => {
-    const body = {
-        ...req.body
-    };
-    console.log(body);
-    const game = new Game(body);
-    game.save().then(result => {
-        res.status(200).json({
-            status: 200,
-            data: {
-                ...req.body
-            }
-        })
-    }).catch(error => {
-        res.status(500).json({
-            error,
-            status: 500
-        });
+    const game = new Game({
+        sportName: req.body.sportName,
+        sportId: req.body.sportId,
+        venue: req.body.venue,
+        venueAddress: req.body.venueAddress,
+        gameDate: req.body.gameDate,
+        description: req.body.description,
+        hostId: req.body.hostId,
+        hostName: req.body.hostName,
     });
+    game.save((err, result) => {
+        if (err) {
+            res.status(500).json({
+                err,
+                status: 500
+            });
+        } else {
+            res.status(200).json({
+                status: 200,
+                data: {
+                    ...result
+                }
+            });
+        }
+    })
 });
 
 module.exports = router;
