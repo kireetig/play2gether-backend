c3d131797b258ded58d545846c8515a9ceaee968
kireeti <kireeti@accendo.com.my>
2019-06-25 15:22:30 +0800

    change in game model

--- api/models/gameModel.js
+++ api/models/gameModel.js
@@ -4,13 +4,15 @@ const Schema = mongoose.Schema;
 
 const gameSchema = new Schema({
     sportName: {type: String, required: true},
+    sportId: {type: Number, required: true},
     venue: {type: String, required: true},
     venueAddress: {type: String},
     gameDate: {type: Date, required: true},
     description: {type: String, required: true},
     hostId: {type: Number, required: true},
     hostName: {type: String},
-    requests: {type: Array}
+    requests: {type: Array},
+    messages: {type: Array}
 });
 
 const Game = mongoose.model('Game', gameSchema);
